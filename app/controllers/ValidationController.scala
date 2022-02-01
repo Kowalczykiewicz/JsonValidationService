6e92643e1d3885604ea6b4c6b591eed83aecaf52
mkowalczykiewicz <michal.kowalczykiewicz@infor.com>
2022-02-01 15:32:50 +0100

    JVS: Added skeleton controllers

--- app/controllers/ValidationController.scala
+++ app/controllers/ValidationController.scala
@@ -1,15 +1,15 @@
 package controllers
 
-import javax.inject._
-import play.api._
 import play.api.mvc._
+
+import javax.inject._
 
 /**
  * This controller creates an `Action` to handle HTTP requests to the
  * application's home page.
  */
 @Singleton
-class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+class ValidationController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
   /**
    * Create an Action to render an HTML page.
@@ -18,8 +18,8 @@ class HomeController @Inject()(cc: ControllerComponents) extends AbstractControl
    * will be called when the application receives a `GET` request with
    * a path of `/`.
    */
-  def index() = Action { implicit request: Request[AnyContent] =>
-    Ok("Json Validation Service ")
+  def validate() = Action { implicit request: Request[AnyContent] =>
+    Ok("validate")
   }
 
   
