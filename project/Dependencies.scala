1f816d7707e4bd61e0355a49ab90aec29a42653e
mkowalczykiewicz <michal.kowalczykiewicz@infor.com>
2022-02-01 17:04:05 +0100

    JVS: Added slick & h2 dep

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,4 +2,7 @@ import sbt._
 
 object Dependencies {
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.0.5"
+  lazy val h2 = "com.h2database" % "h2" % "1.4.192"
+  lazy val slick = "com.typesafe.slick" %% "slick" % "3.3.2"
+
 }
