eb89090b14e6260689d6b2b54f4c41b1419bfd3f
mkowalczykiewicz <michal.kowalczykiewicz@infor.com>
2022-02-03 06:34:45 +0100

    JVS: Play-circe dep

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,18 +1,19 @@
 import sbt._
 
 object Dependencies {
-  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.0.5"
+  lazy val scalaTestPlay = "org.scalatestplus.play" %% "scalatestplus-play" % "5.1.0"
   lazy val h2 = "com.h2database" % "h2" % "1.4.192"
-  lazy val slick = "com.typesafe.slick" %% "slick" % "3.3.2"
+  lazy val slickPlay = "com.typesafe.play" %% "play-slick" % "4.0.0"
 
   lazy val circeDeps = {
+    val playCirce = "com.dripower" %% "play-circe" % "2814.1"
     val circeSchema = "io.circe" %% "circe-json-schema" % "0.1.0"
     val circeVersion = "0.14.1"
     Seq(
       "io.circe" %% "circe-core",
       "io.circe" %% "circe-generic",
       "io.circe" %% "circe-parser"
-    ).map(_ % circeVersion) :+ circeSchema
+    ).map(_ % circeVersion) :+ circeSchema :+ playCirce
 
   }
 }
