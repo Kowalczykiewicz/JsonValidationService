fcbb520d35539b19f3b8a1f1276edf30a6fd12a5
mkowalczykiewicz <michal.kowalczykiewicz@infor.com>
2022-02-03 06:36:22 +0100

    JVS: Added API-info to HelloController

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,13 +1,20 @@
 package controllers
 
 import play.api.mvc._
+
 import javax.inject._
 
 @Singleton
 class HomeController @Inject() (cc: ControllerComponents) extends AbstractController(cc) {
 
-  def index(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
-    Ok("Json Validation Service")
+  def index(): Action[AnyContent] = Action {
+    val api =
+      """
+        |POST    /schema/SCHEMAID        - Upload a JSON Schema with unique `SCHEMAID`
+        |GET     /schema/SCHEMAID        - Download a JSON Schema with unique `SCHEMAID`
+        |POST    /validate/SCHEMAID      - Validate a JSON document against the JSON Schema identified by `SCHEMAID`
+        |""".stripMargin
+    Ok(api)
   }
 
 }
