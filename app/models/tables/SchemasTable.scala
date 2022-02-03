1e70fed6c83e1098a9a1e6b63b0c176a304be014
mkowalczykiewicz <michal.kowalczykiewicz@infor.com>
2022-02-03 06:43:49 +0100

    JVS: Rename

--- app/models/tables/SchemasTable.scala
+++ app/models/tables/SchemasTable.scala
@@ -3,12 +3,12 @@ package models.tables
 import models.{ IdSchema, Schema }
 import slick.jdbc.H2Profile.api._
 
-class Schemas(tag: Tag) extends Table[Schema](tag, "schemas") {
+class SchemasTable(tag: Tag) extends Table[Schema](tag, "schemas") {
   def id = column[IdSchema]("id", O.PrimaryKey)
   def json = column[String]("json")
   def * = (id, json) <> (Schema.tupled, Schema.unapply)
 }
 
-object Schemas {
-  lazy val query = TableQuery[Schemas]
+object SchemasTable {
+  lazy val query = TableQuery[SchemasTable]
 }
